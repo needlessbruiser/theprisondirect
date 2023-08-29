18c3f719db10b479b1358b723331c2f4af41cb25
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-08-29 18:24:38 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Explore Prison Services with The Prison Direct</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Explore Prison Services with The Prison Direct</h1>
+        <p>The Prison Direct offers a comprehensive directory of prison-related services and resources. From facility information to inmate support programs, we provide valuable tools to navigate the world of incarceration. Stay informed and connected with our offerings.</p>
+        <p>For more information, visit the <a href="https://theprisondirect.com">The Prison Direct</a> or find details about <a href="https://theprisondirect.com">State prison</a>.</p>
+
+        <div>
+        </div>
+        <a href="https://www.facebook.com/ahsanghaffar23/" target="_blank"><button>Connect on Facebook</button></a>
+
+    </div>
+</body>
+
+</html>
